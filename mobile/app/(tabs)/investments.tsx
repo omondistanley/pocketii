a13ed8df7f23ea4d1b9606ed3a4900c197e16f21
Stanley Omondi <omondistanley82@gmail.com>
2026-07-15 20:21:09 -0400

    Add disclosure to native investments

--- mobile/app/(tabs)/investments.tsx
+++ mobile/app/(tabs)/investments.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import { InvestmentsOverviewBody } from "../../src/screens/InvestmentsOverviewBody";
+import { FinancialGuidanceNotice } from "../../src/components/FinancialGuidanceNotice";
+import { useAppTheme } from "../../src/theme";
 
 export default function InvestmentsTabScreen() {
-  return <InvestmentsOverviewBody />;
+  const theme = useAppTheme();
+  return (
+    <View style={[styles.screen, { backgroundColor: theme.colors.background }]}>
+      <View style={styles.notice}><FinancialGuidanceNotice compact /></View>
+      <InvestmentsOverviewBody />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: { flex: 1 },
+  notice: { paddingHorizontal: 16, paddingTop: 8 },
+});
